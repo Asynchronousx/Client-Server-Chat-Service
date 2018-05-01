bc540bfe87c785722d880e8e11f805557582da1f
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2018-05-01 11:31:40 +0200

    Update Client.c

--- src/Client.c
+++ src/Client.c
@@ -11,6 +11,8 @@
 #include <netinet/ip.h> 
 #include <sys/socket.h>
 
+#define COLOR_YELLOW "\x1b[33m"
+#define RESET "\x1b[0m"
 #define MESSAGE_LENGHT 64
 #define USER_LENGHT 16
 
@@ -109,7 +111,7 @@ int main(int argc, char * argv[]) {
                     strcpy(DISCONNECTED, otheruser);
                     EXIT_PROCEDURE('R');
                 }
-                printf("%s: %s\n", otheruser, buffer);
+                printf(COLOR_YELLOW "%s: %s" RESET "\n",otheruser, buffer);
                 fflush(stdin);
             }
 
